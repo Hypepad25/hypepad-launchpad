20144fc276d50f1696ea57ca0580af0189341ee0
Hypepad25 <joescyclesound@gmail.com>
2025-07-22 20:43:54 -0500

    update frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,21 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Sidebar from './components/Sidebar/Sidebar';
-import Home from './pages/Home/Home';
-import Launch from './pages/Launch/Launch';
-import Stake from './pages/Stake/Stake';
-import Trending from './pages/Trending/Trending';
-import Promote from './pages/Promote/Promote';
-import Locker from './pages/Locker/Locker';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '4rem' }}>
+      <h1>🔥 Welcome to HYPEPAD</h1>
+      <p>Your all-in-one multi-chain launchpad.</p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Navbar />
-        <div className="main">
-          <Sidebar />
-          <div className="content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/launch" element={<Launch />} />
-              <Route path="/stake" element={<Stake />} />
-              <Route path="/trending" element={<Trending />} />
-              <Route path="/promote" element={<Promote />} />
-              <Route path="/locker" element={<Locker />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
