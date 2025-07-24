1485a68f3e8d0547a7d45cce21627bc921fbc07f
Hypepad25 <joescyclesound@gmail.com>
2025-07-23 20:59:24 -0500

    update navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,6 +7,13 @@ const Navbar = () => (
     <Link to='/create' style={{ marginRight: 15, color: '#fff' }}>Create</Link>
     <Link to='/dashboard' style={{ marginRight: 15, color: '#fff' }}>Dashboard</Link>
     <Link to='/staking' style={{ marginRight: 15, color: '#fff' }}>Staking</Link>
+    <Link to='/locker' style={{ marginRight: 15, color: '#fff' }}>Locker</Link>
+    <Link to='/claim' style={{ marginRight: 15, color: '#fff' }}>Claim</Link>
+    <Link to='/leaderboard' style={{ marginRight: 15, color: '#fff' }}>Leaderboard</Link>
+    <Link to='/promoted' style={{ marginRight: 15, color: '#fff' }}>Promoted</Link>
+    <Link to='/kyc' style={{ marginRight: 15, color: '#fff' }}>KYC</Link>
+    <Link to='/referral' style={{ marginRight: 15, color: '#fff' }}>Referral</Link>
+    <Link to='/settings' style={{ marginRight: 15, color: '#fff' }}>Settings</Link>
   </nav>
 );
 
