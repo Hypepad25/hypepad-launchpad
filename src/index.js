fb09aaf54f0d93747571083d98b7db5843aac97b
Hypepad25 <joescyclesound@gmail.com>
2025-07-24 21:28:36 -0500

    update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './components/Navbar.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
