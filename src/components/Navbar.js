67452801d05d0853849921eb67e2d93d77a73d97
Hypepad25 <joescyclesound@gmail.com>
2025-07-24 21:17:02 -0500

    update src

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 
-const Navbar = () => (
-  <nav style={{ background: '#111', color: '#fff', padding: '10px' }}>
-    <Link to='/' style={{ marginRight: 15, color: '#fff' }}>Home</Link>
-    <Link to='/create' style={{ marginRight: 15, color: '#fff' }}>Create</Link>
-    <Link to='/dashboard' style={{ marginRight: 15, color: '#fff' }}>Dashboard</Link>
-    <Link to='/staking' style={{ marginRight: 15, color: '#fff' }}>Staking</Link>
-    <Link to='/locker' style={{ marginRight: 15, color: '#fff' }}>Locker</Link>
-    <Link to='/claim' style={{ marginRight: 15, color: '#fff' }}>Claim</Link>
-    <Link to='/leaderboard' style={{ marginRight: 15, color: '#fff' }}>Leaderboard</Link>
-    <Link to='/promoted' style={{ marginRight: 15, color: '#fff' }}>Promoted</Link>
-    <Link to='/kyc' style={{ marginRight: 15, color: '#fff' }}>KYC</Link>
-    <Link to='/referral' style={{ marginRight: 15, color: '#fff' }}>Referral</Link>
-    <Link to='/settings' style={{ marginRight: 15, color: '#fff' }}>Settings</Link>
-  </nav>
-);
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import logo from '../assets/logo.png';
+
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          <img src={logo} alt="HypePad Logo" />
+        </Link>
+        <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+          ☰
+        </button>
+        <div className={`navbar-menu ${menuOpen ? 'open' : ''}`}>
+          <Link to="/launch">Launch</Link>
+          <Link to="/stake">Stake</Link>
+          <Link to="/promote">Promote</Link>
+          <Link to="/locker">Locker</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar;
