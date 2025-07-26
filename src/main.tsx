32291e4b42fd01b5150f928070f92b36f4c0a205
Hypepad25 <joescyclesound@gmail.com>
2025-07-26 11:51:56 -0500

    update ui vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,10 @@
-// Main entry for HypePad frontend
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
