e1400e9a0549a043f5e3a9cbce26e630b8b1b775
Hypepad25 <joescyclesound@gmail.com>
2025-07-27 17:10:39 -0500

    ui fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,15 @@
-// App with router
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SubmitAd from './pages/SubmitAd';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/submit-ad" element={<SubmitAd />} />
+      </Routes>
+    </Router>
+  );
+}
