66ad5cd2e157dd84878b492276580a702f77dc2d
Hypepad25 <joescyclesound@gmail.com>
2025-07-27 17:18:23 -0500

    ui update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import SubmitAd from './pages/SubmitAd';
 
 export default function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/submit-ad" element={<SubmitAd />} />
